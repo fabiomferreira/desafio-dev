148a3b846b10f17a451769c6eceb941080e915df
Fabio Ferreira <fabiomataferreira@gmail.com>
2022-06-27 18:14:40 -0400

    Add file upload functionality to api

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require('express');
+const multer = require('multer');
 
+const bodyParser = require('body-parser');
+const storage = multer.memoryStorage()
+const upload =  multer({storage}).single('upload_file');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('API is working!')
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+	res.send('API is working!')
 })
 
+router.post('/uploadFile/', upload, (req, res) => {
+	const {file} = req;
+	const buffer = Buffer.from(file.buffer);
+	console.log(buffer.toString());
+	res.sendStatus(200)
+})
+
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+app.use(bodyParser.json());
+app.use('/v1', router);
+
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
+	console.log(`Server listening on port ${port}`)
 })
