96415bfd452c5aeb7ddc7158a2f6941a244703ad
Fabio Ferreira <fabiomataferreira@gmail.com>
2022-06-29 08:38:20 -0400

    Change transaction value db column type

--- database.sql
+++ database.sql
@@ -2,7 +2,7 @@ CREATE TABLE transactions (
 		transaction_id serial primary key,
     transaction_type varchar(1),
     transaction_date date,
-    transaction_value money,
+    transaction_value NUMERIC(11,2),
     cpf varchar(11),
     card_number varchar(12),
     transaction_time time,
